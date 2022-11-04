5ef18d3885f4c16753706f1753f3128a5828ebd0
Ernesto Hurtado <calerojulio92@gmail.com>
2022-11-04 15:22:25 -0600

    add error handler global

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { NextFunction, Request, Response, Router } from 'express';
 import CategoryRoutes from './category.routes';
 
 export default class Routes {
@@ -11,6 +12,16 @@ export default class Routes {
 
   public get router(): Router {
     this._router.use('/categories', this._categoryRoutes.routes);
+    this._router.use(this.handlerGlobalError);
     return this._router;
+  }
+  private handlerGlobalError(
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    const code404 = 400;
+    res.status(code404).send({ Error: err.message });
   }
 }
