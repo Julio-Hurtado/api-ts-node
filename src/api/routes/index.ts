0c88355df37cc238cb0feff2dd7d5123d04f4eb7
Ernesto Hurtado <calerojulio92@gmail.com>
2022-11-15 17:54:31 -0600

    add routes upload

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,17 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response, Router } from 'express';
 import CategoryRoutes from './category.routes';
+import UploadRoutes from './upload.routes';
 
 export default class Routes {
   private _router: Router;
   private _categoryRoutes: CategoryRoutes;
+  private _uploadRoutes: UploadRoutes;
   constructor() {
     this._router = Router();
     this._categoryRoutes = new CategoryRoutes();
+    this._uploadRoutes = new UploadRoutes();
   }
 
   public get router(): Router {
     this._router.use('/categories', this._categoryRoutes.routes);
+    this._router.use('/upload', this._uploadRoutes.routes);
     this._router.use(this.handlerGlobalError);
     return this._router;
   }
