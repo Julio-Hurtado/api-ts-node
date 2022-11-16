94d252cc64ae6777cb5e662ce7e89823f3aade58
Ernesto Hurtado <calerojulio92@gmail.com>
2022-11-16 16:43:01 -0600

    modified search and delete previous image

--- src/helpers/update-file.ts
+++ src/helpers/update-file.ts
@@ -1,18 +1,16 @@
 import { UploadedFile } from 'express-fileupload';
-import path from 'path';
 import fs from 'fs';
 import { uploadFile } from './upload-file';
+import { getLocalPathImage } from './find-path-image';
 
 export const updateImage = async (
   file: UploadedFile,
   image: string | null,
   folder: string,
 ) => {
-  if (image) {
-    const pathImg = path.join(__dirname, '../uploads', folder, image);
-    if (fs.existsSync(pathImg)) {
-      fs.unlinkSync(pathImg);
-    }
+  const { path } = await getLocalPathImage(image, folder);
+  if (path) {
+    fs.unlinkSync(path);
   }
   const newImage = await uploadFile(file, null, folder);
   return newImage;
