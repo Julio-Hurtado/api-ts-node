ce600dbd318329eaf6adf20a3e19480db48c18a8
Ernesto Hurtado <calerojulio92@gmail.com>
2023-01-21 11:55:55 -0600

    endpoint auth added

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response, Router } from 'express';
+import { AuthRoutes } from './auth.routes';
 import CategoryRoutes from './category.routes';
 import UploadRoutes from './upload.routes';
 
@@ -7,15 +8,18 @@ export default class Routes {
   private _router: Router;
   private _categoryRoutes: CategoryRoutes;
   private _uploadRoutes: UploadRoutes;
+  private _authRoutes: AuthRoutes;
   constructor() {
     this._router = Router();
     this._categoryRoutes = new CategoryRoutes();
     this._uploadRoutes = new UploadRoutes();
+    this._authRoutes = new AuthRoutes();
   }
 
   public get router(): Router {
     this._router.use('/categories', this._categoryRoutes.routes);
     this._router.use('/upload', this._uploadRoutes.routes);
+    this._router.use('/auth', this._authRoutes.routes);
     this._router.use(this.handlerGlobalError);
     return this._router;
   }
